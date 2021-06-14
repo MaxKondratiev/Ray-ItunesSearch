24be5b7a48d225d1b94cb7b0fecd24e1893a026b
MaxKondratiev <78102464+MaxKondratiev@users.noreply.github.com>
2021-06-14 09:00:15 +0300

    Initial Commit

---
+++ Ray-StoreSearch/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ray-StoreSearch
+//
+//  Created by максим  кондратьев  on 14.06.2021.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
